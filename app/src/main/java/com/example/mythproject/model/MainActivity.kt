4df34221a28ad2c1561abdf3c51e98351e17f682
CptNekcmanINT <neckman@yandex.ru>
2022-02-02 02:19:47 +0300

    myths for all categorys

--- app/src/main/java/com/example/mythproject/model/MainActivity.kt
+++ app/src/main/java/com/example/mythproject/model/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.mythproject.model
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.res.AssetManager
-import android.graphics.Color.red
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -26,7 +23,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var navView: NavigationView
 
-    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -52,10 +48,42 @@ class MainActivity : AppCompatActivity() {
                 R.id.menu_greece -> {
                     assetManager("greece")
                     toolbar.title = "Древняя Греция"
+                    toolbar.setBackgroundResource(R.drawable.background_greece)
                 }
                 R.id.menu_rome -> {
                     assetManager("rome")
                     toolbar.title = "Древний Рим"
+                    toolbar.setBackgroundResource(R.drawable.background_rome)
+                }
+                R.id.menu_china -> {
+                    assetManager("china")
+                    toolbar.title = "Древний Китай"
+                    toolbar.setBackgroundResource(R.drawable.background_china)
+                }
+                R.id.menu_egypt -> {
+                    assetManager("egypt")
+                    toolbar.title = "Древний Египет"
+                    toolbar.setBackgroundResource(R.drawable.background_egypt)
+                }
+                R.id.menu_rus -> {
+                    assetManager("rus")
+                    toolbar.title = "Древняя Русь"
+                    toolbar.setBackgroundResource(R.drawable.background_rus)
+                }
+                R.id.menu_japan -> {
+                    assetManager("japan")
+                    toolbar.title = "Древняя Япония"
+                    toolbar.setBackgroundResource(R.drawable.background_japan)
+                }
+                R.id.menu_maya -> {
+                    assetManager("maya")
+                    toolbar.title = "Индейцы Майя"
+                    toolbar.setBackgroundResource(R.drawable.background_maya)
+                }
+                R.id.menu_skandinav -> {
+                    assetManager("scandinav")
+                    toolbar.title = "Скандинавия"
+                    toolbar.setBackgroundResource(R.drawable.background_scandinav)
                 }
             }
             drawerLayout.closeDrawer(GravityCompat.START)
